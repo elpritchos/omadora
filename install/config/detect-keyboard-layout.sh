3b42ccffb1aadc211d6e1a3f519c4d9994d4936f
elpritchos <13980397+elpritchos@users.noreply.github.com>
2025-08-28 13:06:19 +0900

    Set keyboard layout in the correct default file

--- install/config/detect-keyboard-layout.sh
+++ install/config/detect-keyboard-layout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy Fedora X11 keyboard layout to Hyprland config
-hyprconf="$HOME/.config/hypr/hyprland.conf"
+hyprconf="$HOME/.config/hypr/input.conf"
 
 # Get layout and variant using localectl
 layout=$(localectl status | awk -F: '/X11 Layout/ {gsub(/ /,"",$2); print $2}')
