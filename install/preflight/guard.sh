ae266b9a93d4bcaa299475efd26582cec71a5ede
elpritchos <13980397+elpritchos@users.noreply.github.com>
2025-10-17 16:29:29 +0900

    minor guard and network config update to allow mac arch

--- install/preflight/guard.sh
+++ install/preflight/guard.sh
@@ -13,10 +13,14 @@ abort() {
 [[ -f /etc/fedora-release ]] || abort "Fedora"
 
 # Must not be running as root
-[ "$EUID" -eq 0 ] && abort "Running as user (not root)"
+if [ "$EUID" -eq 0 ]; then
+  abort "Running as user (not root)"
+fi
 
-# Must be x86 only to fully work
-[ "$(uname -m)" != "x86_64" ] && abort "x86_64 CPU"
+# Must be x86_64 or aarch64
+if [ "$(uname -m)" != "x86_64" ] && [ "$(uname -m)" != "aarch64" ]; then
+  abort "x86_64 or aarch64 CPU"
+fi
 
 # Should be a core only install
 groups=$(dnf group list --installed --hidden -q | awk 'NR>1 {print $1}')
