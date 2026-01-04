a9accc80ccf363d16e3c0c68b8b8296191073790
elpritchos <13980397+elpritchos@users.noreply.github.com>
2026-01-04 19:57:47 +0900

    update bins

---
+++ install/packaging/webapps.sh
@@ -0,0 +1,3 @@
+omadora-webapp-install "Google Maps" https://maps.google.com "Google Maps.png"
+omadora-webapp-install "ChatGPT" https://chatgpt.com/ ChatGPT.png
+omadora-webapp-install "YouTube" https://youtube.com/ YouTube.png
