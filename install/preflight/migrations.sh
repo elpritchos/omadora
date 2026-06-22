faaf8302de4e07858f211b3fd4364b33d22d77e8
elpritchos <13980397+elpritchos@users.noreply.github.com>
2026-06-22 15:40:25 +0900

    minor install updates

--- install/preflight/migrations.sh
+++ install/preflight/migrations.sh
@@ -1,6 +1,9 @@
-OMADORA_MIGRATIONS_STATE_PATH=~/.local/state/omadora/migrations
-mkdir -p $OMADORA_MIGRATIONS_STATE_PATH
+OMADORA_MIGRATIONS_STATE_PATH="$HOME"/.local/state/omadora/migrations
+mkdir -p "$OMADORA_MIGRATIONS_STATE_PATH"
 
-for file in ~/.local/share/omadora/migrations/*.sh; do
-  touch "$OMADORA_MIGRATIONS_STATE_PATH/$(basename "$file")"
+files=("$HOME"/.local/share/omadora/migrations/*.sh)
+
+for file in "${files[@]}"; do
+  [[ -e "$file" ]] || continue
+  touch "$OMADORA_MIGRATIONS_STATE_PATH/${file##*/}"
 done
