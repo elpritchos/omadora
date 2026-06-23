a86fbb009842d9f2cbdb6ae03b513d69a41bee6d
elpritchos <13980397+elpritchos@users.noreply.github.com>
2026-06-23 14:16:57 +0900

    update font style

--- install/packaging/flatpak.sh
+++ install/packaging/flatpak.sh
@@ -4,3 +4,7 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 # Install default flatpaks
 flatpak install --noninteractive flathub com.dec05eba.gpu_screen_recorder
 flatpak install --noninteractive flathub org.localsend.localsend_app
+
+# Install wrappers
+omadora-flatpak-cmd-install org.localsend.localsend_app localsend
+omadora-flatpak-cmd-install com.dec05eba.gpu_screen_recorder gpu-screen-recorder
