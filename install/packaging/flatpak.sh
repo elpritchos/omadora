3bee18aa1d7ecebc9cca91072b346e9c1380d5ee
elpritchos <13980397+elpritchos@users.noreply.github.com>
2026-06-25 13:51:36 +0900

    add share, update flatpak install

--- install/packaging/flatpak.sh
+++ install/packaging/flatpak.sh
@@ -5,6 +5,9 @@ flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.f
 flatpak install --noninteractive flathub com.dec05eba.gpu_screen_recorder
 flatpak install --noninteractive flathub org.localsend.localsend_app
 
-# Install wrappers
+# Install cli wrappers
 omadora-flatpak-cmd-install org.localsend.localsend_app localsend
 omadora-flatpak-cmd-install com.dec05eba.gpu_screen_recorder gpu-screen-recorder
+
+# Add flatpak overrides
+flatpak override --user --filesystem=home org.localsend.localsend_app
