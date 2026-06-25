1f5881106d9ae6e28986fbb329705b4374a2cc2f
elpritchos <13980397+elpritchos@users.noreply.github.com>
2026-06-25 22:24:57 +0900

    minor fixes

--- default/waybar/indicators/screen-recording.sh
+++ default/waybar/indicators/screen-recording.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if pgrep -f "^gpu-screen-recorder" >/dev/null; then
+if pgrep -f "^gpu-screen-recorder" >/dev/null 2>&1; then
   echo '{"text": "󰻂", "tooltip": "Stop recording", "class": "active"}'
 else
   echo '{"text": ""}'
