03613614908d25b8619bc15256b11a3c00682486
elpritchos <13980397+elpritchos@users.noreply.github.com>
2026-06-30 07:33:24 +0900

    update systemd units

--- install/login/systemd.sh
+++ install/login/systemd.sh
@@ -3,19 +3,4 @@ sudo systemctl set-default graphical.target
 
 # Enable Omadora systemd units
 systemctl --user enable omadora-session.target
-
-systemctl --user enable omadora-battery-monitor.timer
-systemctl --user enable omadora-update-check.timer
-systemctl --user enable omadora-weather-check.timer
-
-systemctl --user enable omadora-fcitx5.service
-systemctl --user enable omadora-hyprland-monitor.service
-systemctl --user enable omadora-polkitagent.service
 systemctl --user enable omadora-recover-internal-monitor.service
-systemctl --user enable omadora-session.service
-
-systemctl --user enable hypridle.service
-systemctl --user enable hyprpaper.service
-systemctl --user enable hyprsunset.service
-systemctl --user enable mako.service
-systemctl --user enable waybar.service
