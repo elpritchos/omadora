1b96f70c7653cec7807d7b024640589254bdf6b6
elpritchos <13980397+elpritchos@users.noreply.github.com>
2026-07-02 23:40:46 +0900

    fix missing option when installing coprs

--- install/preflight/dnf.sh
+++ install/preflight/dnf.sh
@@ -1,4 +1,4 @@
 # Enable necessary COPRs
 sudo dnf copr enable -y sdegler/hyprland
-sudo dnf copr enable jdxcode/mise
-sudo dnf copr enable atim/starship
+sudo dnf copr enable -y jdxcode/mise
+sudo dnf copr enable -y atim/starship
