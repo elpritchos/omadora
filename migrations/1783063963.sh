b35b59b9a1d9729b0f9b5e1befa928d34e7184d9
elpritchos <13980397+elpritchos@users.noreply.github.com>
2026-07-04 07:33:05 +0900

    don't code tired

--- migrations/1783063963.sh
+++ migrations/1783063963.sh
@@ -54,11 +54,9 @@ source_install_script packaging/base.sh
 source_install_script packaging/cargo.sh
 source_install_script packaging/flatpak.sh
 source_install_script packaging/fonts.sh
-source_install_script packaging/npm.sh
-
-echo "Install bundled application icons and TUI launchers"
 source_install_script packaging/icons.sh
 source_install_script packaging/tuis.sh
+source_install_script packaging/npm.sh
 
 echo "Apply updated hardware configuration"
 remove_old_networkd_file() {
